c2157bfb3397a7221e4f501900ffa2d2457a5149
Raphael Orati <misterbroderkill@gmail.com>
2024-08-15 18:35:20 -0300

    Revisão de tipos de variáveis primitivas e adição da classe operadores

--- Tipo-Varaveis/src/Tipos_variaveis.java
+++ Tipo-Varaveis/src/Tipos_variaveis.java
@@ -1,5 +1,23 @@
 public class Tipos_variaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 123;
+        short ano = 2022;
+        int populacao = 2000000;
+        long populacaoBrasil = 2100000000l;
+        float preco = 3.14f;
+        double valor = 3.1415;
+        char letra = 'a';
+        boolean status = true;
+
+        //Exemplo de casting
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        
+        //Constantes
+        final double VALOR_PI = 3.14;
+        System.out.println(VALOR_PI);
+
+        
     }
 }
