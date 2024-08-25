632346cbf38b8adaa7bad404a37fc9fb696ab5e0
Raphael Orati <misterbroderkill@gmail.com>
2024-08-25 17:20:52 -0300

    Inclusão do doWhile

--- EstruturasDeRepeticao/src/ExemploDoWhile.java
+++ EstruturasDeRepeticao/src/ExemploDoWhile.java
@@ -4,7 +4,13 @@ import java.util.Random;
 
 public class ExemploDoWhile {
     public static void main(String[] args) {
+        System.out.println("Discando...");
+        do {
+            System.out.println("Tocando...");
         
+        }while(tocando());
+
+        System.out.println("Aló!");
     }
     private static boolean tocando() {
         boolean atendeu = new Random().nextInt(3) == 1;
