13106809a6e3e06e2ecd7e271c5ee213fe213186
Raphael Orati <misterbroderkill@gmail.com>
2024-08-28 17:28:21 -0300

    Código final do desafio Contador, com abordagem a try/catch e aplicacao de métodos

--- DesafioControleDeFluxo/src/Contador.java
+++ DesafioControleDeFluxo/src/Contador.java
@@ -4,17 +4,21 @@ public class Contador {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
 
-        System.out.println("");
+        System.out.println("Digite o primeiro valor:");
         int num1 = ler.nextInt();
         ler.nextLine();
 
-        System.out.println("");
+        System.out.println("Digite o segundo valor:");
         int num2 = ler.nextInt();
         ler.nextLine();
 
         try{
+            if(num1 >= num2){
+                throw new ParametrosInvalidosException();
+            }
             conta(num1, num2);
         }catch(ParametrosInvalidosException e){
+            System.out.println("Erro: o segundo valor deve ser maior que o primeiro.");
             e.printStackTrace();
         }
         
@@ -22,7 +26,7 @@ public class Contador {
     public static void conta(int parNum1, int parNum2) throws ParametrosInvalidosException{
         int diferenca = parNum2 - parNum1;
             
-        for(int i = 0; i<=diferenca; i++){
+        for(int i = 1; i<diferenca+1; i++){
                 System.out.println(i);
         }
     }
--- DesafioControleDeFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleDeFluxo/src/ParametrosInvalidosException.java
@@ -1,1 +1,2 @@
-public class ParametrosInvalidosException extends Exception {}
+public class ParametrosInvalidosException extends Exception {
+}
