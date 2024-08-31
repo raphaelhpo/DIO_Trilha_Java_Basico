5961f0d22a3f09e586f1a638df38c8d7dccf14a5
Raphael Orati <misterbroderkill@gmail.com>
2024-08-30 21:44:40 -0300

    Conteúdo Sobre Classes

---
+++ Programacao_OOP/src/Classes.java
@@ -0,0 +1,35 @@
+public class Classes {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        /*
+         * Todo código fonte é um .java, e toda classe tem um .class
+         * Que serão executadas pela JVM quando o programa é executado.
+         * Propriedade da classe:
+         *   Classe(Class);
+         *      Estrutura e ou representação que direciona a criação dos
+         *      objetos;
+         *   Identificador(identity);
+         *      Próposito existencial aos objetos criados;
+         *   Características(states);
+         *      Características de estado, também denominados de atributos;
+         *   Comportamento(behaviors);
+         *      Também conhecidos com ações ou métodos;
+         *   instância(new);
+         *      Representação de um objeto, o ató de materializar uma;
+         * 
+         * Tipos de Classes:
+         *    Classe de modelo(model);
+         *       Classes que representam a estrutura de domínio da aplicação;
+         *    Classe de serviço(Service);
+         *       Classes que representam o comportamento da aplicação, regras de negócio;
+         *    Classe de repositório(Repository);
+         *       Classes que representam a persistência de dados, integração com banco;
+         *    Classe de controle(Controller);
+         *       Classes que representam a interação externa via http, web ou webservice;
+         *    Classe utilitária(Util);
+         *       Classes que contém recursos comuns à toda aplicação;
+         * 
+         */
+    }
+}
+
