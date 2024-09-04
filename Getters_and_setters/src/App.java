9e8fa1abfb6d716af39e2433e3ad7d8642bb8439
Raphael Orati <misterbroderkill@gmail.com>
2024-09-04 20:20:21 -0300

    Getters Ans Setters Conteúdo e explicação

---
+++ Getters_and_setters/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        /*
+         * Os métodos Getters e setters são utilizados para acessar as propriedades de uma classe;
+         * Getters: Retorna o valor da propriedade;
+         * Setters: Define o valor da propriedade.
+         * 
+         */
+    }
+}
---
+++ Getters_and_setters/src/Escola/Escola.java
@@ -0,0 +1,11 @@
+package Escola;
+
+public class Escola {
+    public static void main(String args[]){
+        Aluno Aluno_001 = new Aluno();
+        Aluno_001.setNome("Raphael");
+        Aluno_001.setIdade(23);
+
+        System.out.println("O nome do Aluno e: " + Aluno_001.nome + "\nA idade do aluno e: " + Aluno_001.idade);
+    }
+}
