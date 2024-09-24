53b417bdfe1cab894697c03b5bfa5c03701b8678
Raphael <misterbroderkill@gmail.com>
2024-09-24 18:56:47 -0300

    Resolução do execício de List em collections 24092024

--- Collections/src/List/Exercicios_list/ListaTarefas.java
+++ Collections/src/List/Exercicios_list/ListaTarefas.java
@@ -4,35 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListaTarefas {
-    List<Tarefa> tarefas = new ArrayList<>();
+    List<Tarefa> tarefas;
+
+    public ListaTarefas(){
+        this.tarefas = new ArrayList<>();
+    }
 
     /**@param tarefa descrição da tarefa
      * @return null Adiciona um valor à lista.
      **/
-    public void adicionarTarefa(String tarefa){ 
-        Tarefa f = new Tarefa(tarefa);
-        tarefas.add(f);
+    public void adicionarTarefa(String descricao){
+        tarefas.add(new Tarefa(descricao));
     }
-
 
     public void removerTarefa(String descricao){
         //Remove uma tarefa da lista com base em sua descrição.
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
         
+        for (Tarefa t : tarefasParaRemover) {
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefas.removeAll(tarefasParaRemover);
     }
 
     public int obterNumeroTotalTarefas(){
-        //Retorna o número total de tarefas na lista.
         return tarefas.size();
     }
 
     public void obterDescricaoesTarefas(){
         //Retorna uma lista contendo a descrição de todas as tarefas na lista.
-    }
-}
-class Tarefa{
-    String descricao;
-
-    public Tarefa(String descricao){
-        this.descricao = descricao;
+        System.out.println(tarefas);
     }
 }
