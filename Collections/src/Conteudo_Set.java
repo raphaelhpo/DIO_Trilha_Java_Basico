70d6370ac43487eb63a44152c41c5f2cb96a5737
Raphael <misterbroderkill@gmail.com>
2024-10-01 14:10:06 -0300

    AJustes de contéudo
    Signed-off-by: Raphael <misterbroderkill@gmail.com>

---
+++ Collections/src/Conteudo_Set.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Conteudo_Set {
+    public static void main(String[] args) {
+        List<Conteudo_Set> conteudos = new ArrayList<>();
+    }
+}
