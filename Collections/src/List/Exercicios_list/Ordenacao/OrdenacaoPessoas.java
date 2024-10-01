583c4490e9b58f4c146b99de68255b2f19190996
Raphael <misterbroderkill@gmail.com>
2024-10-01 15:43:52 -0300

    Ajuste de Package 'Convidados'
    Signed-off-by: Raphael <misterbroderkill@gmail.com>

--- Collections/src/List/Exercicios_list/Ordenacao/OrdenacaoPessoas.java
+++ Collections/src/List/Exercicios_list/Ordenacao/OrdenacaoPessoas.java
@@ -6,24 +6,6 @@ import java.util.List;
 
 public class OrdenacaoPessoas {
     List<Pessoa> pessoas;
-    public static void main(String[] args) {
-        // Criando uma instância da classe OrdenacaoPessoas
-          OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
-      
-          // Adicionando pessoas à lista
-          ordenacaoPessoas.adicionarPessoa("Alice", 20, 1.56);
-          ordenacaoPessoas.adicionarPessoa("Bob", 30, 1.80);
-          ordenacaoPessoas.adicionarPessoa("Charlie", 25, 1.70);
-          ordenacaoPessoas.adicionarPessoa("David", 17, 1.56);
-      
-          // Exibindo a lista de pessoas adicionadas
-          System.out.println(ordenacaoPessoas.pessoas);
-          // Ordenando e exibindo por idade
-          System.out.println(ordenacaoPessoas.ordenarPorIdade());
-      
-          // Ordenando e exibindo por altura
-          System.out.println(ordenacaoPessoas.ordenarPorAltura());
-    }
 
     public OrdenacaoPessoas(){
         this.pessoas = new ArrayList<>();
@@ -43,5 +25,22 @@ public class OrdenacaoPessoas {
         pessoasPorAltura.sort(new ComparatorPorAltura());
         return pessoasPorAltura;
     }
-
+    public static void main(String[] args) {
+        // Criando uma instância da classe OrdenacaoPessoas
+          OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+      
+          // Adicionando pessoas à lista
+          ordenacaoPessoas.adicionarPessoa("Alice", 20, 1.56);
+          ordenacaoPessoas.adicionarPessoa("Bob", 30, 1.80);
+          ordenacaoPessoas.adicionarPessoa("Charlie", 25, 1.70);
+          ordenacaoPessoas.adicionarPessoa("David", 17, 1.56);
+      
+          // Exibindo a lista de pessoas adicionadas
+          System.out.println(ordenacaoPessoas.pessoas);
+          // Ordenando e exibindo por idade
+          System.out.println(ordenacaoPessoas.ordenarPorIdade());
+      
+          // Ordenando e exibindo por altura
+          System.out.println(ordenacaoPessoas.ordenarPorAltura());
+    }
 }
