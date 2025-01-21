0126a36a387a29e2269a98cf762b2e8c32361df6
Raphael Orati <misterbroderkill@gmail.com>
2025-01-20 21:52:23 -0300

    Inclusão de CRUD em paginas WEB.

--- Spring/Projeto_APIREST/web_spring/CalculadoraController.java
+++ Spring/Projeto_APIREST/web_spring/CalculadoraController.java
@@ -1,5 +1,6 @@
 package br.com.orati.springapi.web_spring.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
--- Spring/Projeto_APIREST/web_spring/src/main/java/br/com/orati/springapi/web_spring/controller/ProdutoController.java
+++ Spring/Projeto_APIREST/web_spring/src/main/java/br/com/orati/springapi/web_spring/controller/ProdutoController.java
@@ -1,15 +1,19 @@
 package br.com.orati.springapi.web_spring.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.orati.springapi.web_spring.model.entities.Produto;
 import br.com.orati.springapi.web_spring.model.repositories.ProdutoRepository;
-
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
@@ -18,11 +22,23 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
     
-    @PostMapping(path = "/novo")
-    public @ResponseBody Produto novoProduto(@RequestParam String nome){
-        Produto produto = new Produto(nome);
+    @RequestMapping(method= {RequestMethod.POST, RequestMethod.PUT})
+    public @ResponseBody Produto novoProduto(@Valid Produto produto) {
         produtoRepository.save(produto);
         return produto;
     }
+    @GetMapping(path = "/listar")
+    public Iterable<Produto> ListarProdutos() {
+        return produtoRepository.findAll();
+    }
 
+    @GetMapping(path = "/procurar/{id}")
+    public Optional<Produto> Procurar(@PathVariable int id) {
+        return produtoRepository.findById(id);
+    }
+
+    @DeleteMapping(path="/{id}")
+    public void excluirProduto(@PathVariable int id) {
+        produtoRepository.deleteById(id);
+    }
 }
