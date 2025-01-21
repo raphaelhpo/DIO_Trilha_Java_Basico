f9d701ca0a7c8c0345011cf19958a69e75ce8d19
Raphael Orati <misterbroderkill@gmail.com>
2025-01-20 22:19:26 -0300

    Ajustando erros e criando Paginação.

--- Spring/Projeto_APIREST/web_spring/src/main/java/br/com/orati/springapi/web_spring/controller/ProdutoController.java
+++ Spring/Projeto_APIREST/web_spring/src/main/java/br/com/orati/springapi/web_spring/controller/ProdutoController.java
@@ -3,6 +3,8 @@ package br.com.orati.springapi.web_spring.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.orati.springapi.web_spring.model.entities.Produto;
 import br.com.orati.springapi.web_spring.model.repositories.ProdutoRepository;
 import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("/produtos")
@@ -31,6 +34,14 @@ public class ProdutoController {
     public Iterable<Produto> ListarProdutos() {
         return produtoRepository.findAll();
     }
+
+    @GetMapping(path = "/pagina/{pagina}")
+    public Iterable<Produto> ObterProdutosPorPagina(@PathVariable int pagina) {
+        Pageable pageRequest = PageRequest.of(pagina, 3);
+        // Consultar os produtos com paginação
+        return produtoRepository.findAll(pageRequest);
+    }
+    
 
     @GetMapping(path = "/procurar/{id}")
     public Optional<Produto> Procurar(@PathVariable int id) {
