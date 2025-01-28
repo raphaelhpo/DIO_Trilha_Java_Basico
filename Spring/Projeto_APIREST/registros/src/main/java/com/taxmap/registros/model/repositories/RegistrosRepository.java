7a52e62a7d50e13ce9e1a676c78d55bb3bc684d0
Raphael Orati <raphael.orati@taxmaptributos.com.br>
2025-01-28 14:31:09 -0300

    Tentativa de ajuste

--- Spring/Projeto_APIREST/registros/src/main/java/com/taxmap/registros/model/repositories/RegistrosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.taxmap.registros.model.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.taxmap.registros.model.entities.Registros;
-
-public interface RegistrosRepository extends JpaRepository<Registros, Long> {
-
-}
