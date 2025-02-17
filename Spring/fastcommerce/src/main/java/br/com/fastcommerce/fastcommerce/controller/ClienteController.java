6c66c4d1adcb6c24e2f89056d9b1e4b9028027da
Raphael Orati <misterbroderkill@gmail.com>
2025-02-17 10:04:36 -0300

    ajuste .gitIgnore

--- Spring/fastcommerce/src/main/java/br/com/fastcommerce/fastcommerce/controller/ClienteController.java
+++ Spring/fastcommerce/src/main/java/br/com/fastcommerce/fastcommerce/controller/ClienteController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fastcommerce.fastcommerce.model.Cliente;
 import br.com.fastcommerce.fastcommerce.service.ClienteService;
-import feign.Response;
 
 @RestController
 @RequestMapping("clientes")
