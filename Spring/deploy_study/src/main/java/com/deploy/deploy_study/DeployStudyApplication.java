b31a43f27c7d9e11548b9a73c3cccdee2d620c67
Raphael Orati <misterbroderkill@gmail.com>
2025-03-04 00:44:51 -0300

    Configuração de .yml e inclusão de anottations adicionais

--- Spring/deploy_study/src/main/java/com/deploy/deploy_study/DeployStudyApplication.java
+++ Spring/deploy_study/src/main/java/com/deploy/deploy_study/DeployStudyApplication.java
@@ -2,12 +2,12 @@ package com.deploy.deploy_study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.deploy.deploy_study.model")
 public class DeployStudyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DeployStudyApplication.class, args);
 	}
-
 }
--- Spring/deploy_study/src/main/java/com/deploy/deploy_study/model/Cliente.java
+++ Spring/deploy_study/src/main/java/com/deploy/deploy_study/model/Cliente.java
@@ -1,18 +1,22 @@
 package com.deploy.deploy_study.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "cliente", schema="public")
 public class Cliente {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
     private String username;
     private String password;
